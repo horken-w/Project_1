b52b038a53457a1ca7068a9e4162a2a81b4c5358
horkenW <horkenwong@gmail.com>
2015-09-04 19:04:53 +0800

    20140904 menu list
    right, left side menu build success,
    bug: item can't change tag so move the other tree will still be a group
    and hot to serialize to be JSON

--- menuitem.js
+++ menuitem.js
@@ -1,24 +1,48 @@
 $(function(){
 	$.ajax({
-		url: 'menuJSON.json',
+		url: 'menuJSON(userlist).json',
 		type: 'post',
 		datatype: 'json',
 		success: setmenu
 	})
-	$('#menulist').nestable({
-		group: 1,
-		maxDepth: 1
+	$('#menulistLeft').nestable({
+		group: 1
 	});
-		
-
+	$('#menulistRight').nestable({
+		group: 1,
+		maxDepth: 2
+	});	
 })
 function setmenu(jmenu){
 	var list=$.parseJSON(jmenu);
 
-	$.each(list, function(i, val){
-		$('#menulist').append(menuList(val));
+	$.each(list.menuList, function(i, val){
+		var $menu=menuList(val)
+		$('#menulistLeft').append($menu);
 	})
+	if(list.userList.length <= 0)
+		$('<div class="dd-empty"></div>').appendTo('#menulistRight');
+	else{
+		$.each(list.userList, function(i, val){
+			$('#menulistRight').append('<ol class="dd-list">'+modelList(val)+'</ol>');
+		})
+	}
 }
+function modelList(item){
+	var buildmenu = '<li class="dd-item dd3-item" data-id="' + item.linklistid + '">';
+		buildmenu += '<div class="dd-handle dd3-handle">DragHere</div><div class="dd3-content" name=" menu' + item.linklistid + '">' + item.name + '</div>';
+		if(item.menu && item.menu.length>0){
+			buildmenu+='<ol class="dd-list">';
+			$.each(item.menu, function(i, sub){
+				buildmenu+= modelList(sub);
+			});
+			buildmenu+='</ol>'
+			console.log(item.menu)
+		}
+	buildmenu+='</li>'
+	return buildmenu;
+}
+
 function menuList(item){
 	var buildmenu = '<li class="dd-item dd3-item" data-id="' + item.linklistid + '">';
 		buildmenu += '<div class="dd-handle dd3-handle">DragHere</div><div class="dd3-content" name=" menu' + item.linklistid + '">' + item.name + '</div></li>';
